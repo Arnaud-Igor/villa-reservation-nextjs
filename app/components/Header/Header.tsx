1637f0a77afa65232b73530dd3f7193a9734c953
MacBook Pro <macbookpro@192.168.1.12>
2024-09-26 12:31:51 +0000

    first commit

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -12,7 +12,7 @@ function Header() {
       <div className="absolute bg-black inset-0 opacity-70"></div>
       <div className="container relative text-white text-center space-y-7 md:space-y-10">
         <h1 className="text-xl md:text-6xl font-bold">{`Nos Villas d'Exception`}</h1>
-        <p className="text-base md:text-2xl">
+        <p className="text-xs md:text-2xl">
           Chacune de nos villas est soigneusement sélectionnée pour répondre aux
           attentes des voyageurs les plus exigeants. Des intérieurs raffinés,
           des équipements de prestige et des emplacements privilégiés vous
