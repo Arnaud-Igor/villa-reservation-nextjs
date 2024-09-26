bb22e3a99043ced5a25ae5c1497d874883956f47
MacBook Pro <macbookpro@192.168.1.12>
2024-09-26 18:03:16 +0000

    first commit

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center gap-4 text-sm text-gray-600 border-t py-4 mt-6">
+      <p>&copy; 2024 Villa Location. All rights reserved.</p>
+      <p>{`ARNAUD IGOR N'TAKPE | DEVELOPPEUR WEB JUNIOR`}</p>
+    </div>
+  );
 }
 
 export default Footer;
